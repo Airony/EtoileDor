8935b9cc53698e72a7130fc7d9010c0a27bb956a
Airony <dxangusx@gmail.com>
2025-02-26 14:31:01 +0100

    feat(backend): add function to query reservations by day

---
+++ backend/src/utils/queryReservationsByDay.ts
@@ -0,0 +1,24 @@
+import { Payload } from "payload";
+
+/**
+ * Queries reservations by a specific day.
+ *
+ * @param payload - The payload object .
+ * @param day - The day for which reservations are to be queried, in ISO8601 string format.
+ * @returns A promise that resolves to an array of reservations.
+ */
+export default async function queryReservationsByDay(
+    payload: Payload,
+    day: string,
+) {
+    return (
+        await payload.find({
+            collection: "reservations",
+            where: {
+                day: {
+                    equals: day,
+                },
+            },
+        })
+    ).docs;
+}

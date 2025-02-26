a3ee8baee29f90b1b76d59d425e63692b76490ef
Airony <dxangusx@gmail.com>
2025-02-26 14:21:32 +0100

    fix(backend): update validateReservationTime.ts to work with minutes past midnight time format

--- backend/src/utils/validateReservationTime.ts
+++ backend/src/utils/validateReservationTime.ts
@@ -1,13 +1,11 @@
 export default function validateReservationTime(time: number): boolean {
-    // Suppose reservations start at 11 am and end at 9 pm
-    // In 15 minute intervals
-    // Time should be in military time
-    // Todo : Have start and end times be configurable
-    if (time < 1100 || time > 2100) {
-        return false;
-    }
+    const minutesIncrements = 30; // TODO : This should be a globally defined constant
+    const reservationTimesStart = 660;
+    const reservationTimesEnd = 1260;
 
-    if (time % 15 !== 0) {
-        return false;
-    }
+    return (
+        time >= reservationTimesStart &&
+        time <= reservationTimesEnd &&
+        time % minutesIncrements === 0
+    );
 }
